a7128d0f27a6aa0f07770cd0f1b5425f32a95342
Anthony Babka <anthonybabka@gmail.com>
2022-06-28 21:31:48 -0700

    added styling and upcoming pages

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,11 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index(): 
     return render_template('index.html', pets=pets)
+
+@bp.route('/show')
+def show():
+    return render_template('show.html')
+
+@bp.route('/facts')
+def facts():
+    return render_template('facts.html')
